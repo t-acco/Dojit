1b16a60dbb5e2f37399be2cbd934df6c840c5613
tacco <tacco@simplee.com>
2016-09-15 12:10:26 +0300

    change-prod

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,4 +1,4 @@
-if Rails.env.development?
+#if Rails.env.development?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address:        'smtp.sendgrid.net',
@@ -9,4 +9,4 @@ if Rails.env.development?
     domain:         'heroku.com',
     enable_starttls_auto: true
   }
-end
+#end
