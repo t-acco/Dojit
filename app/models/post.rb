985dda00d6ddca38c316f981795f1735186d01da
tacco <tacco@simplee.com>
2016-09-15 10:40:05 +0300

    connect posts to user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ActiveRecord::Base
   include ActiveModel::ForbiddenAttributesProtection
-  attr_accessible :body, :title
+  attr_accessible :body, :title, :user
 
   has_many :comments
+  belongs_to :user
+
+  default_scope { order('created_at DESC') }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
    attr_accessible :email, :password, :password_confirmation, :name
+   has_many :posts
 end
