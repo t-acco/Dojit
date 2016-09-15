dcf3856c5cbae44281544a9d01d9d27d928c3e60
tacco <tacco@simplee.com>
2016-09-15 11:06:17 +0300

    change host production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,10 @@ Dojit::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { host: 'https://tom-dojit.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
